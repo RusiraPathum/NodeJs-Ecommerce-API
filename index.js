f155278e8bc9d5b55a0546db8ff21980496df28e
RusiraPathum <rusira.pathum20@gmail.com>
2022-02-17 19:51:50 +0530

    end point testing

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hi Pathum")
+//import express module
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// create instance from express
+const app = express();
+
+//connect database
+mongoose.connect(process.env.MONGO_URL)
+    .then(() =>
+        console.log("DB Connection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.get("/api/test", () => {
+    console.log("Testing successfull")
+})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running");
+});
